1c0245dbb296896ea71a8d3c21611c16dfe7714f
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-26 21:43:17 -0300

    mensagem de encerramento

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -17,5 +17,6 @@ public class TesteEmpregado {
         System.out.println(e1.calcularSalario());
         System.out.println(e2.calcularSalario());
         System.out.println(e3.calcularSalario());
+        System.out.println ("Terminando..");
     }   
 }
